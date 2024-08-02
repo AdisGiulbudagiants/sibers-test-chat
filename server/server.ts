32072c4da0586a408cc1a9aca951697de6380dbb
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-08-02 22:08:41 +0700

    Test project v1

---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import { config } from "dotenv"
+config()
+
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import { userRoutes } from "./routes/users"
+
+const app = fastify()
+app.register(cors, { origin: "*" })
+app.register(userRoutes)
+
+app
+  .listen({ port: parseInt(process.env.PORT!) })
+  .then(() => console.log("Server is running"))
